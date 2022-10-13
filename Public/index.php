576abc4b6bb3a7ab0f27b316002afba3290ccf49
creeksgie <t.piveteau1@laposte.net>
2022-10-13 14:21:37 +0200

    index.php

---
+++ Public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+ <head>
+    <meta charset='utf-8'>
+    <link rel="stylesheet" href="stylesheet.css">
+    <?php  include("..\Donnees.inc.php"); ?>
+ </head>
+ <body>
+   <header>
+      yo
+   </header>
+   <nav>
+   </nav>
+   <main>
+   </main>
+   <footer>
+      salut
+   </footer>
+ </body>
+</html>
