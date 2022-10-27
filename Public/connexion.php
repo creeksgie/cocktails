4173f1648baa5aaa86aeff87653908c321cee793
MrLuigi57 <MrLuigi@github.com>
2022-10-27 14:42:06 +0200

    1er version Zone de Connexion - Luc LETULE

---
+++ Public/connexion.php
@@ -0,0 +1,64 @@
+<span id="login">
+<?php
+    function est_vide($chaine)
+    {
+        return(trim($chaine)=='');
+    }
+
+    $complet=true;
+    if(isset($_GET["submit"]))
+    {
+        if(   !est_vide($_GET["login"])
+           && !est_vide($_GET["mdp"])
+           && !est_vide($_GET["nom"])
+            && !est_vide($_GET["prenom"])
+            && !est_vide($_GET["dateN"])
+           )
+        {
+            $_SESSION['user']['nom'] =$_GET["nom"];
+            $_SESSION['user']['prenom'] = $_GET["prenom"];
+            $_SESSION['user']['dateN'] = $_GET["dateN"];
+            $complet = true;
+        }
+    }
+
+    if($complet)
+    {
+        include('index.php');
+    }
+    else{
+        if(isset($_GET['submit']))
+        {
+            echo 'Veuillez compléter tous les champs si vous plait';
+        }
+        else{
+            echo 'Veuillez remplir le formulaire';
+        }
+    }
+?>
+</span>
+
+<main>
+    <form method="get" action="?page=connexion">
+        Votre login :
+        <input type="text" name="login"
+               value="<?php echo (isset($_GET['login'])?$_GET['login']:'');?>"> <br />
+
+        Votre Mot de Passe :
+        <input type="text" name="mdp"
+               value="<?php echo (isset($_GET['mdp'])?$_GET['mdp']:'');?>"> <br />
+
+        Votre Nom :
+        <input type="text" name="nom"
+               value="<?php echo (isset($_GET['nom'])?$_GET['nom']:'');?>"> <br />
+
+        Votre Prenom :
+        <input type="text" name="prenom"
+               value="<?php echo (isset($_GET['prenom'])?$_GET['prenom']:'');?>"> <br />
+
+        Votre Date de Naissance :
+        <input type="date" name="dateN"
+               value="<?php echo (isset($_GET['dateN'])?$_GET['dateN']:'');?>"> <br />
+    </form>
+</main>
+
--- Public/index.php
+++ Public/index.php
@@ -19,16 +19,24 @@ session_start();
    <header>
       <button onclick=window.location.href="?page=Aliment" ;>Navigation</button>
       <button>Recettes <img class="svg" src="..\svg\coeurplein.svg" alt=""> </button>
-      <span id="login">
-         Zone de connection
-      </span>
+      <?php require("connexion.php"); ?>
       <span id="search">
          Recherche : <input id="searchbar" type="text" placeholder="">
          <button><img class="svg" src="..\svg\loupe.svg" alt=""></button>
       </span>
    </header>
    <nav>
-      <?php require("navigation.php"); ?>
+      <?php
+      /*if(isset($_SESSION['user']))
+      {
+          if(!isset($_GET['page'])) $_GET['page']='index';
+          if(in_array($_GET['page'],array('')))
+          {
+              include($_GET['page'].".php");
+          }
+      }
+      else include('connexion.php');*/
+      ?>
    </nav>
    <main>
       <?php
