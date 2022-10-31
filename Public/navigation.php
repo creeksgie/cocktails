f459f0bd56a5b5534309757175646c3dc1186df4
creeksgie <t.piveteau1@laposte.net>
2022-10-31 20:01:44 +0100

    re

--- Public/navigation.php
+++ Public/navigation.php
@@ -1,45 +1,46 @@
 <article>
-    <h2>Aliment Courant</h2>
+    <p>Aliment Courant</p>
     <span>
-    <?php
-    if (!isset($_GET['page']) || $_GET['page'] == 'Aliment') {
-        $Lien = $Hierarchie['Aliment'];
-    ?>
-        <a href="?page=Aliment">Aliment</a> /
         <?php
-        $chemin[] = "Aliment";
-    } else {
-        $chemin = $_SESSION['chemin'];
-        $i = count($chemin);
-        $Lien = $Hierarchie[$_GET['page']];
-        $chemin[] = $_GET['page'];
-        if ($chemin[$i] == $chemin[$i - 1]) {
-            array_splice($chemin, $i);
-            $i--;
-        }
-        if ($i >= 3) {
-            if ($chemin[$i] == $chemin[$i - 2] ||  $chemin[$i] == $chemin[$i - 3]) {
-                do {
-                    array_splice($chemin, $i);
-                    $i--;
-                } while ($chemin[$i] != $_GET['page']);
+        if (!isset($_GET['page']) || $_GET['page'] == 'Aliment') {
+            $Lien = $Hierarchie['Aliment'];
+        ?>
+            <a href="?page=Aliment">Aliment</a> /
+            <?php
+            $chemin[] = "Aliment";
+        } else {
+            $chemin = $_SESSION['chemin'];
+            $i = count($chemin);
+            $Lien = $Hierarchie[$_GET['page']];
+            $chemin[] = $_GET['page'];
+            if ($chemin[$i] == $chemin[$i - 1]) {
+                array_splice($chemin, $i);
+                $i--;
+            }
+            if ($i >= 3) {
+                if ($chemin[$i] == $chemin[$i - 2] ||  $chemin[$i] == $chemin[$i - 3]) {
+                    do {
+                        array_splice($chemin, $i);
+                        $i--;
+                    } while ($chemin[$i] != $_GET['page']);
+                }
+            }
+            foreach ($chemin as $index_chemin => $page) {
+            ?>
+                <a href="?page=<?php echo $page; ?>"><?php echo $page; ?></a> /
+        <?php
             }
         }
-        foreach ($chemin as $index_chemin => $page) {
+
+        $_SESSION['chemin'] = $chemin;
+
         ?>
-            <a href="?page=<?php echo $page; ?>"><?php echo $page; ?></a> /
-    <?php
-        }
-    }
-
-    $_SESSION['chemin'] = $chemin;
-
-    ?>
     </span>
     <?php
     foreach ($Lien as $clé => $val) {
         if ($clé != 'super-categorie') {
-    ?><h2><?= htmlentities($clé) ?></h2>
+    ?>
+            <p><?= htmlentities($clé) ?></p>
             <ul><?php
                 foreach ($val as $clé2 => $val2) {
                 ?> <li><a href="?page=<?php echo $Lien[$clé][$clé2]; ?>"><?php echo $Lien[$clé][$clé2]; ?></a></li>
